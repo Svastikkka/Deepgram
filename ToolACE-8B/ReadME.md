842634fadaad5aed1744cf8a150296f25d6235e8
Manshu Sharma <manshusharma@Manshus-MacBook-Air.local>
2025-02-07 11:31:41 +0530

    add: example setup of ToolACE-8B

---
+++ ToolACE-8B/ReadME.md
@@ -0,0 +1,7 @@
+1. System Requirements:
+- Hardware: A GPU with sufficient VRAM (at least 16GB is recommended) to handle the 8B parameter model.
+- Software: Ensure you have Python installed (version 3.8 or higher).
+
+
+
+pip install accelerate>=0.26.0 torch transformers
---
+++ ToolACE-8B/example.py
@@ -0,0 +1,10 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "Team-ACE/ToolACE-8B"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype='auto',
+    device_map='auto'
+)
